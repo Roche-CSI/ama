8ad5d71c70e1bcedffc3cb9594ce8dc3ef5745fa
dhars9-roche <saurav.dhar@contractors.roche.com>
2025-12-19 11:38:17 -0800

    add methods to set data and active project with optional persistence

--- client/amapy-core/amapy_core/configs/app_settings.py
+++ client/amapy-core/amapy_core/configs/app_settings.py
@@ -108,6 +108,12 @@ class AppSettings:
         self._data = x
         FileUtils.write_json(data=self._data, abs_path=self.settings_file)
 
+    def set_data(self, x, persist=True):
+        self._data = x
+        if persist:
+            # update the settings file
+            FileUtils.write_json(data=self._data, abs_path=self.settings_file)
+
     @property
     def assets_home(self) -> str:
         try:
@@ -309,10 +315,14 @@ class AppSettings:
             self._active_project = self.data.get('active_project') or None  # default is null
             return self._active_project
 
-    @active_project.setter
-    def active_project(self, x: str):
-        self._active_project = x
-        self.data = utils.update_dict(self.data, {'active_project': self._active_project})
+    # @active_project.setter
+    # def active_project(self, x: str):
+    #     self._active_project = x
+    #     self.data = utils.update_dict(self.data, {'active_project': self._active_project})
+
+    def set_active_project(self, project_id: str, persist=True):
+        self._active_project = project_id
+        self.set_data(utils.update_dict(self.data, {'active_project': self._active_project}), persist)
 
     @property
     def active_project_data(self) -> dict:
