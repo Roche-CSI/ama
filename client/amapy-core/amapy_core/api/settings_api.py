674eefb59de63bf98484116e5e91afcac5c73a47
dhars9-roche <saurav.dhar@contractors.roche.com>
2026-03-04 00:35:42 -0800

    Add command to fetch asset versions from remote

--- client/amapy-core/amapy_core/api/settings_api.py
+++ client/amapy-core/amapy_core/api/settings_api.py
@@ -205,6 +205,16 @@ class SettingsAPI(LoggingMixin):
             message += colored_string("Signed in as: {}".format(colored_string(res.get("user").get("username"),
                                                                                LogColors.INFO)))
             self.user_log.message(message)
+
+            # set the dashboard url if available in the response
+            dashboard_url = res.get("dashboard_url")
+            if dashboard_url:
+                cfg = self.settings.shared().user_configs
+                cfg.update({"dashboard_url": dashboard_url})
+                cfg.save()
+
+                self.user_log.message(f"Dashboard url is set to: {dashboard_url}")
+
             if self.settings.active_project:
                 projects = self.print_all_projects(jsonize=jsonize)
                 if jsonize:
