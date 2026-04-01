276891a2cff35d031d494f7014d9486810da8192
dhars9-roche <saurav.dhar@contractors.roche.com>
2026-04-01 13:33:50 -0700

    fix: Update test to use mock GCS credentials for CORS configuration

--- client/amapy-plugin-gcs/amapy_plugin_gcs/tests/test_bucket_cors.py
+++ client/amapy-plugin-gcs/amapy_plugin_gcs/tests/test_bucket_cors.py
@@ -1,9 +1,9 @@
 from amapy_plugin_gcs.bucket_cors import update_cors_configuration
 
 
-def test_add():
-    credentials = {}
+def test_update_cors_configuration(mock_gcs_credentials):
     bucket_name = "my_test_bucket"
     origin_url = "http://localhost:3000"
-    bucket = update_cors_configuration(credentials, bucket_name, origin_url)
+
+    bucket = update_cors_configuration(mock_gcs_credentials, bucket_name, origin_url)
     print(f"bucket :{bucket}")
