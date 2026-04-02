975d0095a2fc94fe5e0e6b37c0f8064e26a03b67
Saurav Dhar <saurav.dhar@contractors.roche.com>
2026-04-02 13:47:03 -0700

    CHORE: Update amapy-plugin-gcr to Python 3.12 (#68)
    Update amapy-plugin-gcr for Python 3.12.
    
    - Update requires-python to >=3.12,<3.13
    - Update setuptools build requirement to 74.x
    - Update google-auth from 2.15.* to 2.37.*
    - Removed unused dependencies
    - Test coverage 83%
    
    ---------
    
    Co-authored-by: Dhar, Saurav {DSRI~SANTA CLARA} <saurav.dhar+roche@contractors.roche.com>
    Co-authored-by: Ona <no-reply@ona.com>

--- client/amapy-plugin-gcr/amapy_plugin_gcr/transporter/gcr_transporter.py
+++ client/amapy-plugin-gcr/amapy_plugin_gcr/transporter/gcr_transporter.py
@@ -3,15 +3,15 @@ from amapy_pluggy.storage.transporter import Transporter, TransportResource
 
 class GcrTransporter(Transporter):
 
-    def upload(self, resources: [TransportResource]):
+    def upload(self, resources: list[TransportResource]):
         # no upload or download allowed currently
         raise NotImplementedError
 
-    def download(self, resources: [TransportResource]):
+    def download(self, resources: list[TransportResource]):
         # no upload or download allowed currently
         # todo: use docker to download the image
         raise NotImplementedError
 
-    def copy(self, resources: [TransportResource]):
+    def copy(self, resources: list[TransportResource]):
         # no copy allowed currently
         raise NotImplementedError
