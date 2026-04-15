6ff3bd9fbaa7efd082ad44bb0505eb29d169f982
dhars9-roche <saurav.dhar@contractors.roche.com>
2026-04-15 12:10:52 -0700

    CHORE: Refactor commit message handling in commit.py for improved API usage

--- client/amapy/amapy/commands/asset_actions/commit.py
+++ client/amapy/amapy/commands/asset_actions/commit.py
@@ -1,28 +1,25 @@
-from functools import cached_property
-
 from amapy.commands import CliAction, CliOption
-from amapy_core.api.repo_api import AssetAPI, AddAPI
+from amapy_core.api.repo_api import AssetAPI
 
 
 class CommitMessage(CliAction):
     name = "commit"
     help_msg = "commit message for the changes"
 
-    @cached_property
-    def api(self) -> AddAPI:
-        return AssetAPI(self.repo).add
-
     def run(self, args):
         if args.message:
-            self.api.add_commit_message(message=args.message)
+            api = AssetAPI(self.repo).add
+            with api.environment():
+                api.add_commit_message(message=args.message)
         else:
             self.user_log.message("missing commit message, please use asset commit -m <message text>")
 
-    def get_options(self):
+    def get_options(self) -> list[CliOption]:
         return [
-            CliOption(dest="message",
-                      help_msg="commit message",
-                      short_name="m",
-                      full_name="message"
-                      )
+            CliOption(
+                dest="message",
+                help_msg="commit message",
+                short_name="m",
+                full_name="message"
+            )
         ]
