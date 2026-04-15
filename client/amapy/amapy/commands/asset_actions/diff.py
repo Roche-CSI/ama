41218097315ad2fa34e45473515ab7babd011dc5
dhars9-roche <saurav.dhar@contractors.roche.com>
2026-04-15 12:07:11 -0700

    CHORE: Refactor option definitions for consistency in add.py and diff.py

--- client/amapy/amapy/commands/asset_actions/diff.py
+++ client/amapy/amapy/commands/asset_actions/diff.py
@@ -7,33 +7,36 @@ class AssetDiff(CliAction):
     help_msg = "Displays the differences between two versions of an asset"
 
     def run(self, args):
-        print(f"args: {args.__dict__}")
         api = AssetAPI(self.repo).diff
         with api.environment():
             api.diff(file=args.file, src_ver=args.src_ver, dst_ver=args.dst_ver, html=args.html)
 
-    def get_options(self):
+    def get_options(self) -> list[CliOption]:
         return [
-            CliOption(dest="src_ver",
-                      short_name="s",
-                      full_name="src",
-                      help_msg="src version",
-                      n_args="?",
-                      ),
-            CliOption(dest="dst_ver",
-                      short_name="d",
-                      full_name="dst",
-                      help_msg="dst version",
-                      n_args="?",
-                      ),
-            CliOption(dest="file",
-                      short_name="f",
-                      full_name="file",
-                      help_msg="file path",
-                      n_args="?",
-                      ),
-            CliOption(dest="html",
-                      help_msg="if true diff is shown as html",
-                      is_boolean=True
-                      )
+            CliOption(
+                dest="src_ver",
+                short_name="s",
+                full_name="src",
+                help_msg="src version",
+                n_args="?",
+            ),
+            CliOption(
+                dest="dst_ver",
+                short_name="d",
+                full_name="dst",
+                help_msg="dst version",
+                n_args="?",
+            ),
+            CliOption(
+                dest="file",
+                short_name="f",
+                full_name="file",
+                help_msg="file path",
+                n_args="?",
+            ),
+            CliOption(
+                dest="html",
+                help_msg="if true diff is shown as html",
+                is_boolean=True
+            )
         ]
