313daa3289cf4db85f642d033e9df719697bb48b
dhars9-roche <saurav.dhar@contractors.roche.com>
2026-04-15 15:16:05 -0700

    CHORE: Remove unused test functions from test_artifact.py for improved clarity

--- client/amapy/amapy/python_api/tests/test_artifact.py
+++ client/amapy/amapy/python_api/tests/test_artifact.py
@@ -80,32 +80,3 @@ def test_read_file(asset):
         with item.open() as f:
             file_contents = f.read()
             assert file_contents is not None
-
-
-def test_find_alias():
-    # find with alias
-    asset_name = Artifact.find(class_name="swarup_data", alias="group_object_proxy_test")
-    assert asset_name == "swarup_data/8"
-
-
-def test_find_hash():
-    # find with hash
-    asset_names = Artifact.find(class_name="swarup_data", hash="12adc50b32d57b3d17cc829e4cd02c2b")
-    assert asset_names and asset_names[0] == "swarup_data/1/0.0.0"
-
-
-def test_clone():
-    artifact = Artifact.clone(name="swarup_data/1", path="/Users/mahantis/am_demo/swarup_data/1")
-    print(artifact)
-
-
-def test_sort_key():
-    asset_names = ["swarup_data/3/2.1.9", "swarup_data/3/2.10.0", "swarup_data/1/10.0.2"]
-
-    def sort_key(name):
-        # Split the string by '/' and extract parts
-        parts = name.split("/")
-        return parts[0], int(parts[1]), tuple(map(int, parts[2].split(".")))
-
-    asset_names.sort(key=sort_key)
-    assert asset_names[-1] == "swarup_data/3/2.10.0"
