526210fb3a6787c3fbe32abd1bc1f7d4f54a5102
dhars9-roche <saurav.dhar@contractors.roche.com>
2026-04-20 14:59:25 -0700

    revert

--- server/amapy-server/amapy_server/asset_client/objects/object_set.py
+++ server/amapy-server/amapy_server/asset_client/objects/object_set.py
@@ -2,9 +2,9 @@ import os
 from collections.abc import Callable
 
 from amapy_server.asset_client.objects.object import Object
+from amapy_server.utils import string_to_timestamp
 from amapy_server.utils.better_set import BetterSet
 from amapy_server.utils.file_utils import FileUtils
-from amapy_server.utils import string_to_timestamp
 
 
 class ObjectSet(BetterSet):
