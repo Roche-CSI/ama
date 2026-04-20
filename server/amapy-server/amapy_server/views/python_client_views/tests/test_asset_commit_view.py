3e48d9ac854394e12805d6703a4221cf334d7d3a
dhars9-roche <saurav.dhar@contractors.roche.com>
2026-04-20 13:56:54 -0700

    CHORE: Update import path for time_it utility in test_asset_commit_view.py

--- server/amapy-server/amapy_server/views/python_client_views/tests/test_asset_commit_view.py
+++ server/amapy-server/amapy_server/views/python_client_views/tests/test_asset_commit_view.py
@@ -3,8 +3,8 @@ import os
 
 import pytest
 
-from amapy_server.utils import time_it
 from amapy_server.utils.file_utils import FileUtils
+from amapy_utils.utils.utils import time_it
 
 
 @pytest.fixture(scope="module")
