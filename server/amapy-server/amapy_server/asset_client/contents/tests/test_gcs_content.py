74e2645e9ba45646fa07d924f32fee059dcce5f5
dhars9-roche <saurav.dhar@contractors.roche.com>
2026-05-13 13:52:12 -0700

    FIX: Update test data in test_gcs_content.py for consistency

--- server/amapy-server/amapy_server/asset_client/contents/tests/test_gcs_content.py
+++ server/amapy-server/amapy_server/asset_client/contents/tests/test_gcs_content.py
@@ -1,9 +1,9 @@
 from amapy_server.asset_client.contents.gcs_content import GcsContent
 
 DATA = {
-    "id": "gs:md5_placeholder_md5_000==",
+    "id": "gs:md5_1Ab2Cd3Ef4Gh5Ij6Kl7mno==",
     "mime_type": "text/plain",
-    "hash": "md5_placeholder_md5_000==",
+    "hash": "md5_1Ab2Cd3Ef4Gh5Ij6Kl7mno==",
     "size": 56839,
     "meta": {
         "src": "gs://bucket/test/client/asset_classes/00000000-000f-00b0-b0f0-0000000000a0.yaml",
@@ -17,9 +17,9 @@ DATA = {
 def test_can_stage(client_asset):
     asset = client_asset
     content = GcsContent.de_serialize(asset, DATA)
-    assert content.id == "gs:md5_2Gg70COLj3pnRdwuI50ijg=="
-    assert content.hash == "md5_2Gg70COLj3pnRdwuI50ijg=="
+    assert content.id == "gs:md5_1Ab2Cd3Ef4Gh5Ij6Kl7mno=="
+    assert content.hash == "md5_1Ab2Cd3Ef4Gh5Ij6Kl7mno=="
     assert content.hash_type == "md5"
-    assert content.hash_value == "2Gg70COLj3pnRdwuI50ijg=="
+    assert content.hash_value == "1Ab2Cd3Ef4Gh5Ij6Kl7mno=="
     assert not content.can_stage
     assert not content.can_commit
