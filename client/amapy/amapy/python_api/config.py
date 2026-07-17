fbf525370061ab549a07fe0c6b7dd031e383c6a8
dhars9-roche <saurav.dhar@contractors.roche.com>
2026-07-17 16:58:31 -0700

    Add persist option to set method for configurable settings

--- client/amapy/amapy/python_api/config.py
+++ client/amapy/amapy/python_api/config.py
@@ -1,7 +1,10 @@
+import logging
 from functools import cached_property
 from typing import Any
 
 from amapy_core.api.settings_api import SettingsAPI
+
+logger = logging.getLogger(__name__)
 
 
 class Config(object):
@@ -21,7 +24,7 @@ class Config(object):
         """
         return self._api.print_user_configs(jsonize=True)
 
-    def set(self, key: str, value: Any):
+    def set(self, key: str, value: Any, persist: bool = False):
         """Sets custom configuration options.
 
         Parameters
@@ -30,8 +33,16 @@ class Config(object):
             The key for the configuration option.
         value : Any
             The value for the configuration option.
+        persist : bool, optional
+            Set to True to update the config file, otherwise just update the data.
         """
-        return self._api.set_user_configs({key: value})
+        if persist:
+            logger.warning("The settings file will be modified. This can cause issues if you have multiple "
+                           "instances of AMA running on your machine or running AMA with multi-processing.")
+        else:
+            logger.warning("Update is temporary and will not persist across sessions. "
+                           "To make it permanent, set persist=True.")
+        return self._api.set_user_configs({key: value}, persist=persist)
 
     def reset(self, *keys):
         """Resets the configuration to the default value.
